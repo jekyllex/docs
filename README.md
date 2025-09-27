78bbab28dcc35ba5a9d7b29d7ec9c6ada507360b
Gourav Khunger <gouravkhunger18@gmail.com>
2025-09-28 02:16:28 +0530

    feat: init documentation

---
+++ README.md
@@ -0,0 +1,5 @@
+# JekyllEx Docs
+
+Documentation for JekyllEx - the mobile IDE to create, build, edit, preview, and publish Jekyll sites.
+
+[Visit >](https://docs.jekyllex.xyz)
---
+++ astro.config.mjs
@@ -0,0 +1,52 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  // trailingSlash: "never",
+  // site: "https://docs.jekyllex.xyz",
+  site: "https://jekyllex.github.io",
+  base: "/docs/", 
+  server: {
+    port: 4000,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@/": new URL("./src/", import.meta.url).pathname,
+      }
+    }
+  },
+	integrations: [
+		starlight({
+      lastUpdated: true,
+			title: "JekyllEx Docs",
+      favicon: "/favicon.png",
+			social: [
+        { icon: "information", label: "GitHub", href: "https://jekyllex.xyz" },
+        { icon: "github", label: "GitHub", href: "https://github.com/jekyllex" },
+      ],
+			sidebar: [
+				{
+					label: "Getting Started",
+					autogenerate: { directory: "getting-started" },
+				},
+				{
+					label: "Usage",
+					autogenerate: { directory: "usage" },
+				},
+				{
+					label: "Other",
+					autogenerate: { directory: "other" },
+				},
+			],
+      customCss: [
+        "./src/styles/custom.css",
+      ],
+      editLink: {
+        baseUrl: "https://github.com/jekyllex/docs/edit/main/",
+      },
+		}),
+	],
+});
